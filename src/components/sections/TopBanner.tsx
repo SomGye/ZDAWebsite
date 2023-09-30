f10d7fbf2eb339954251d4defd00caf644c4b27a
SomGye <crawfordmaxwell24@gmail.com>
2023-09-30 19:08:56 -0400

    feat: add init Sx for TopBanner Box

--- src/components/sections/TopBanner.tsx
+++ src/components/sections/TopBanner.tsx
@@ -18,40 +18,43 @@ import { useRecoilState, useRecoilValue } from "recoil";
 import { themeAtom } from "../../states/ThemeAtom";
 import { pageAtom } from "../../states/PageAtom";
 import ThemeToggle from "../ThemeToggle";
+import { TopBannerBoxSx } from "./TopBannerSx";
 
 const TopBanner = () => {
   const [page, setPage] = useRecoilState(pageAtom);
 
   return (
     <Container>
-      <Box>
-        <div>Hamburger</div>
-      </Box>
-      <Box>
-        <div>Logo + Title - {page}</div>
-      </Box>
-      <Box>
-        <Button variant="solid">Request A Commission</Button>
-      </Box>
-      {/* TODO: Routing on Navigation, so that users can bookmark or go directly to a page */}
-      <Box>
-        {/* TODO: Make the current page underline the text of matching button */}
-        {/* TODO: Make justify-content: right when desktop */}
-        {/* TODO: Make flex-direction: column when mobile */}
-        <ButtonGroup
-          aria-label="Top Banner Button Group"
-          color="primary"
-          variant="soft"
-          sx={{ justifyContent: "center" }}
-        >
-          <Button onClick={() => setPage("Home")}>Home</Button>
-          <Button onClick={() => setPage("Portfolio")}>Portfolio</Button>
-          {/* TODO: Make the Commissions Button stand out more */}
-          <Button onClick={() => setPage("Commissions")}>Commissions</Button>
-        </ButtonGroup>
-      </Box>
-      <Box>
-        <ThemeToggle />
+      <Box sx={TopBannerBoxSx}>
+        <Box>
+          <div>Hamburger</div>
+        </Box>
+        <Box>
+          <div>Logo + Title - {page}</div>
+        </Box>
+        <Box>
+          <Button variant="solid">Request A Commission</Button>
+        </Box>
+        {/* TODO: Routing on Navigation, so that users can bookmark or go directly to a page */}
+        <Box>
+          {/* TODO: Make the current page underline the text of matching button */}
+          {/* TODO: Make justify-content: right when desktop */}
+          {/* TODO: Make flex-direction: column when mobile */}
+          <ButtonGroup
+            aria-label="Top Banner Button Group"
+            color="primary"
+            variant="soft"
+            sx={{ justifyContent: "center" }}
+          >
+            <Button onClick={() => setPage("Home")}>Home</Button>
+            <Button onClick={() => setPage("Portfolio")}>Portfolio</Button>
+            {/* TODO: Make the Commissions Button stand out more */}
+            <Button onClick={() => setPage("Commissions")}>Commissions</Button>
+          </ButtonGroup>
+        </Box>
+        <Box>
+          <ThemeToggle />
+        </Box>
       </Box>
     </Container>
   );
---
+++ src/components/sections/TopBannerSx.ts
@@ -0,0 +1,10 @@
+export const TopBannerBoxSx = {
+  ...{
+    display: "flex",
+    flexDirection: "row",
+    flexWrap: "nowrap",
+    alignContent: "center",
+    justifyContent: "space-between",
+    alignItems: "center",
+  },
+};
