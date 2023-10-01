a8236786ad3a9cfa61ababe99c45341afe66fc6e
SomGye <crawfordmaxwell24@gmail.com>
2023-09-30 20:07:36 -0400

    fix: better icon and iconbutton colors for footer with Sx

---
+++ src/components/sections/FooterSectionSx.ts
@@ -0,0 +1,22 @@
+export const FooterIconButtonDarkSx = {
+  ...{
+    backgroundColor: "#39343a",
+    borderColor: "#3d393f",
+    "&:hover": {
+      borderColor: "#4d394f",
+      backgroundColor: "#4d394f",
+    },
+  },
+};
+
+export const FooterIconButtonLightSx = {
+  ...{
+    backgroundColor: "#fcfcfc",
+  },
+};
+
+export const FooterIconDarkSx = {
+  ...{
+    color: "#fafafa",
+  },
+};
