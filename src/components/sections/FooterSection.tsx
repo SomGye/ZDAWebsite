e64cafb105618ddc572d532895b03edfc272aea9
Maxwell Crawford <crawfordmaxwell24@gmail.com>
2023-11-20 20:01:11 -0500

    Feat: Footer Section v1 (#11)
    * fix: abstractify title and desc use light for tablet and mobile
    
    * feat: Footer icon btns to full btns and new sx
    
    * feat: Footer sx and elements v1
    
    * tweak: updated privacy policy link

--- src/components/sections/FooterSection.tsx
+++ src/components/sections/FooterSection.tsx
@@ -1,58 +1,57 @@
-// TODO: Footer Section Component
-/**
- * Footer section, same for all pages.
- * Composed of:
- * - Same background as Top Info Section
- * - Title text: "Thank you for visiting!"
- * - Body text: "If you need to contact me, the best way is through email (zerodayanubis@gmail.com) or Discord (@zerodayanubis)."
- *    "If you have any questions, feel free to ask, I am here to help."
- *    "Have a great day!"
- * - Link buttons:
- *   - Email: `mailto:zerodayanubis@gmail.com`
- *   - Discord: https://discordapp.com/users/193548282264420354
- * - Text link to Privacy Policy: https://drive.google.com/file/d/1H4WkDEfEIdqIAKem2sjGiyHJd5wb7udh/view?usp=sharing
- * - Copyright text: "© ZDA WORKS. ALL RIGHTS RESERVED."
- */
 import * as React from "react";
 import { useRecoilValue } from "recoil";
 import { themeAtom } from "../../states/ThemeAtom";
-import { Box, Container, IconButton } from "@mui/joy";
-import { EmailRounded, MessageRounded } from "@mui/icons-material";
+import { Box, Button, Container, Divider, Typography } from "@mui/joy";
+import {
+  AssignmentIndRounded,
+  EmailRounded,
+  MessageRounded,
+} from "@mui/icons-material";
 import { clickEmail, clickLink } from "../../Helpers";
 import {
+  FooterBodyDarkSx,
+  FooterBodyLightSx,
+  FooterBoxSx,
+  FooterContainerSx,
+  FooterCopyrightDarkSx,
+  FooterCopyrightLightSx,
+  FooterDividerSx,
+  FooterIconButtonBoxSx,
   FooterIconButtonDarkSx,
   FooterIconButtonLightSx,
   FooterIconDarkSx,
+  FooterTitleDarkSx,
+  FooterTitleLightSx,
 } from "./FooterSectionSx";
+import "./FooterSection.css";
 
 const FooterSection = () => {
   const theme = useRecoilValue(themeAtom);
 
   return (
-    <Container>
-      <Box>
-        <h3>
-          <b>Thank you for visiting!</b>
-        </h3>
-        <h4>
-          If you need to contact me, the best way is through email
+    <Container className="FooterContainer" sx={FooterContainerSx}>
+      <Box sx={FooterBoxSx}>
+        <Typography
+          level="title-md"
+          sx={theme === "dark" ? FooterTitleDarkSx : FooterTitleLightSx}
+        >
+          Thank you for visiting!
+        </Typography>
+        <Typography
+          level="body-md"
+          sx={theme === "dark" ? FooterBodyDarkSx : FooterBodyLightSx}
+        >
+          If you need to contact me, you can reach me through email
           (zerodayanubis@gmail.com) or Discord (@zerodayanubis).
           <br />
           If you have any questions, feel free to ask, I am here to help.
-          <br />
-          Have a great day!
-        </h4>
-        <Box
-          sx={{
-            display: "flex",
-            gap: 2,
-            flexWrap: "wrap",
-            alignItems: "center",
-            justifyContent: "center",
-          }}
-        >
-          <IconButton
-            variant="outlined"
+        </Typography>
+        <Box sx={FooterIconButtonBoxSx}>
+          <Button
+            variant="solid"
+            startDecorator={
+              <EmailRounded sx={theme === "dark" ? FooterIconDarkSx : {}} />
+            }
             sx={
               theme === "dark"
                 ? FooterIconButtonDarkSx
@@ -60,11 +59,14 @@ const FooterSection = () => {
             }
             onClick={clickEmail}
           >
-            {theme === "dark" && <EmailRounded sx={FooterIconDarkSx} />}
-            {theme !== "dark" && <EmailRounded />}
-          </IconButton>
-          <IconButton
-            variant="outlined"
+            {" "}
+            Email Me
+          </Button>
+          <Button
+            variant="solid"
+            startDecorator={
+              <MessageRounded sx={theme === "dark" ? FooterIconDarkSx : {}} />
+            }
             sx={
               theme === "dark"
                 ? FooterIconButtonDarkSx
@@ -74,14 +76,34 @@ const FooterSection = () => {
               clickLink("https://discordapp.com/users/193548282264420354")
             }
           >
-            {theme === "dark" && <MessageRounded sx={FooterIconDarkSx} />}
-            {theme !== "dark" && <MessageRounded />}
-          </IconButton>
+            {" "}
+            Message Me
+          </Button>
         </Box>
-        <a href="https://drive.google.com/file/d/1H4WkDEfEIdqIAKem2sjGiyHJd5wb7udh/view?usp=sharing">
-          <h5>Privacy Policy</h5>
-        </a>
-        <h6>© ZDA WORKS. ALL RIGHTS RESERVED.</h6>
+        <Divider inset="none" sx={FooterDividerSx} />
+        <Button
+          aria-label="Privacy Policy"
+          title="Privacy Policy"
+          variant="solid"
+          startDecorator={
+            <AssignmentIndRounded
+              sx={theme === "dark" ? FooterIconDarkSx : {}}
+            />
+          }
+          sx={
+            theme === "dark" ? FooterIconButtonDarkSx : FooterIconButtonLightSx
+          }
+          onClick={() => clickLink("https://tinyurl.com/ZDAPrivacy")}
+        >
+          {" "}
+          Privacy Policy
+        </Button>
+        <Typography
+          level="body-xs"
+          sx={theme === "dark" ? FooterCopyrightDarkSx : FooterCopyrightLightSx}
+        >
+          © ZDA WORKS. ALL RIGHTS RESERVED.
+        </Typography>
       </Box>
     </Container>
   );
