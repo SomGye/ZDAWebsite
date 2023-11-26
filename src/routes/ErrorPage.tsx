810cac5bf6ecb7e399f0860addfcc9d971490384
SomGye <crawfordmaxwell24@gmail.com>
2023-11-26 12:11:50 -0500

    tweak: only show ErrorPage body when countdownReady

--- src/routes/ErrorPage.tsx
+++ src/routes/ErrorPage.tsx
@@ -39,21 +39,23 @@ const ErrorPage = () => {
 
   return (
     <Container className="ErrorContainer">
-      <Box
-        sx={{
-          display: "flex",
-          flexDirection: "column",
-          justifyContent: "center",
-          alignItems: "center",
-        }}
-      >
-        <Typography level="title-lg" sx={{ color: "#fafafa" }}>
-          404: Page Not Found
-        </Typography>
-        <Typography level="title-sm" sx={{ color: "#fafafaea" }}>
-          Redirecting in {countdown}...
-        </Typography>
-      </Box>
+      {countdownReady && (
+        <Box
+          sx={{
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <Typography level="title-lg" sx={{ color: "#fafafa" }}>
+            404: Page Not Found
+          </Typography>
+          <Typography level="title-sm" sx={{ color: "#fafafaea" }}>
+            Redirecting in {countdown}...
+          </Typography>
+        </Box>
+      )}
     </Container>
   );
 };
