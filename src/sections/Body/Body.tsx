98987282d9adcf7463a03e1232a19ce2c9485bf9
SomGye <crawfordmaxwell24@gmail.com>
2024-02-12 13:10:50 -0500

    feat: Tailblocks header with correct alignment and base sx

--- src/sections/Body/Body.tsx
+++ src/sections/Body/Body.tsx
@@ -6,23 +6,18 @@ import { themeAtom } from "../../states/themeAtom";
 const Body = () => {
   const theme = useRecoilValue(themeAtom);
   return (
-    <>
-      <div>
-        <img src={zerodayanubis_redpink} className="max-h-12 my-4" />
-      </div>
+    <div className="body-container my-12 flex flex-col justify-center items-center">
+      <img src={zerodayanubis_redpink} className="max-h-12 my-4" />
       <h1 className="font-outfit font-light text-2xl my-4 text-neutral-800 dark:text-neutral-100">
         ZDAWebsite
       </h1>
-      <p className={theme === "dark" ? "nav-link-animated-dark" : "nav-link-animated"}>
-        Page Nav Link
-      </p>
       <button
         className="inline-flex items-center px-4 py-[6px] 3xl:px-5 3xl:py-[8px] 4xl:px-7 4xl:py-[11px] 4k:px-9 4k:py-[14px] 3xl:scale-[1.1] 4xl:scale-[1.2] 4k:scale-[1.4] rounded-md bg-zdaBlue-600 text-[15px] 3xl:text-lg 4xl:text-xl 4k:text-2xl text-gray-200 font-medium transition ease-out duration-300 motion-reduce:transition-none hover:text-gray-100 hover:bg-zdaBlue-800 active:bg-zdaBlue-900/90 select-none"
         onClick={() => window.location.replace("/logo")}
       >
         Logo Page
       </button>
-    </>
+    </div>
   );
 };
 
--- src/sections/Header/Header.tsx
+++ src/sections/Header/Header.tsx
@@ -11,115 +11,97 @@ const Header = () => {
   const theme = useRecoilValue(themeAtom);
   return (
     <>
-      {/* TAILBLOCKS VERS */}
       {/* TODO: mobile centered logo vers */}
-      <span className="text-gray-700 dark:text-gray-200 text-base font-outfit">
-        <div className="container mx-auto flex p-5 flex-col md:flex-row items-center">
-          <a className="flex text-xl font-medium items-center text-gray-900 mb-4 md:mb-0">
-            <div className="zda-header-logo-box md:mr-1">
-              <img
-                src={theme === "dark" ? zdaDarkCircLogo : zdaLightCircLogo}
-                alt="ZDA Z Logo"
-                title="Click to see ZDA logo page"
-                className="zda-header-logo cursor-pointer pointer-events-auto select-none transition ease-out duration-300 3xl:w-[36px] 3xl:h-[36px] 4xl:w-[44px] 4xl:h-[44px] 4k:w-[60px] 4k:h-[60px]"
-                width={36}
-                height={36}
-                onClick={() => window.location.replace("/logo")}
-                onTouchEnd={() => window.location.replace("/logo")}
-              />
-              <img
-                src={
+      {/* TODO: hamburger menu icon button and Joy UI side panel */}
+      <header
+        className="fixed w-full z-40 top-0 left-0 py-5 flex items-center bg-gradient-to-t from-zdaRedpink-400/5 dark:from-zdaRedpink-700/5 backdrop-blur-2xl text-gray-700 dark:text-gray-200 text-base font-outfit border-b border-gray-200/50 dark:border-stone-800/10 rounded-md"
+      >
+        <div
+          className="inline-flex items-center py-3 mx-auto"
+        >
+          {/* Logo/Title/Nav Container */}
+          <div className="header-left flex absolute left-0 ml-4">
+            {/* Logo Btn and Title */}
+            <a className="flex text-xl font-medium items-center text-gray-900 mb-4 md:mb-0">
+              <div className="zda-header-logo-box md:mr-1">
+                <img
+                  src={theme === "dark" ? zdaDarkCircLogo : zdaLightCircLogo}
+                  alt="ZDA Z Logo"
+                  title="Click to see ZDA logo page"
+                  className="zda-header-logo cursor-pointer pointer-events-auto select-none transition ease-out duration-300 3xl:w-[36px] 3xl:h-[36px] 4xl:w-[44px] 4xl:h-[44px] 4k:w-[60px] 4k:h-[60px]"
+                  width={36}
+                  height={36}
+                  onClick={() => window.location.replace("/logo")}
+                  onTouchEnd={() => window.location.replace("/logo")}
+                />
+                <img
+                  src={
+                    theme === "dark"
+                      ? zdaLightCircHoverLogo
+                      : zdaDarkCircHoverLogo
+                  }
+                  alt="ZDA Z Logo Hover Version"
+                  title="Click to see ZDA logo page"
+                  className="zda-header-logo-hover cursor-pointer pointer-events-auto select-none transition ease-out duration-300 3xl:w-[36px] 3xl:h-[36px] 4xl:w-[44px] 4xl:h-[44px] 4k:w-[60px] 4k:h-[60px]"
+                  width={36}
+                  height={36}
+                  onClick={() => window.location.replace("/logo")}
+                  onTouchEnd={() => window.location.replace("/logo")}
+                />
+              </div>
+              <span className="ml-4 mr-2 text-xl text-gray-700 dark:text-gray-300 font-light cursor-default select-none">
+                ⌞ZeroDayAnubis⌝
+              </span>
+            </a>
+            {/* Nav Links */}
+            <nav className="md:mr-auto md:ml-4 md:py-1 md:pl-4 md:border-l md:border-gray-400	flex items-center text-base font-plusjakartasans font-semibold justify-center">
+              <p
+                className={
                   theme === "dark"
-                    ? zdaLightCircHoverLogo
-                    : zdaDarkCircHoverLogo
+                    ? "nav-link-animated-dark"
+                    : "nav-link-animated"
                 }
-                alt="ZDA Z Logo Hover Version"
-                title="Click to see ZDA logo page"
-                className="zda-header-logo-hover cursor-pointer pointer-events-auto select-none transition ease-out duration-300 3xl:w-[36px] 3xl:h-[36px] 4xl:w-[44px] 4xl:h-[44px] 4k:w-[60px] 4k:h-[60px]"
-                width={36}
-                height={36}
-                onClick={() => window.location.replace("/logo")}
-                onTouchEnd={() => window.location.replace("/logo")}
-              />
+              >
+                Home
+              </p>
+              <p
+                className={
+                  theme === "dark"
+                    ? "nav-link-animated-dark"
+                    : "nav-link-animated"
+                }
+              >
+                Portfolio
+              </p>
+              <p
+                className={
+                  theme === "dark"
+                    ? "nav-link-animated-dark"
+                    : "nav-link-animated"
+                }
+              >
+                Commissions
+              </p>
+              <p
+                className={
+                  theme === "dark"
+                    ? "nav-link-animated-dark"
+                    : "nav-link-animated"
+                }
+              >
+                About
+              </p>
+            </nav>
+          </div>
+          {/* Theme Menu Container */}
+          <div className="header-right flex absolute right-0 mr-4">
+            {/* Theme Menu */}
+            <div className="cursor-pointer focus-visible:outline-none mx-2 lg:flex lg:static lg:w-auto">
+              <ThemeMenu />
             </div>
-            <span className="ml-4 mr-2 text-xl text-gray-700 dark:text-gray-300 font-light cursor-default select-none">
-              ⌞ZeroDayAnubis⌝
-            </span>
-          </a>
-          <nav className="md:mr-auto md:ml-4 md:py-1 md:pl-4 md:border-l md:border-gray-400	flex items-center text-base font-plusjakartasans font-semibold justify-center">
-            <p
-              className={
-                theme === "dark"
-                  ? "nav-link-animated-dark"
-                  : "nav-link-animated"
-              }
-            >
-              Home
-            </p>
-            <p
-              className={
-                theme === "dark"
-                  ? "nav-link-animated-dark"
-                  : "nav-link-animated"
-              }
-            >
-              Portfolio
-            </p>
-            <p
-              className={
-                theme === "dark"
-                  ? "nav-link-animated-dark"
-                  : "nav-link-animated"
-              }
-            >
-              Commissions
-            </p>
-            <p
-              className={
-                theme === "dark"
-                  ? "nav-link-animated-dark"
-                  : "nav-link-animated"
-              }
-            >
-              About
-            </p>
-          </nav>
-          <div className="cursor-pointer focus-visible:outline-none mx-2 lg:flex lg:static lg:w-auto">
-            <ThemeMenu />
           </div>
         </div>
-      </span>
-      {/* ZDAWorks VERS */}
-      {/* <span className="z-40 max-h-[18vh] fixed left-0 top-0 flex w-full items-center justify-between border-b border-gray-200 rounded-md bg-gradient-to-t from-zdaRedpink-500/5 p-4 backdrop-blur-2xl dark:border-neutral-900 dark:bg-zdaBG-darkCard dark:from-zdaRed-600/ lg:flex text-zdaText-dark dark:text-zdaText-light cursor-default select-none">
-        <div className="zda-header-logo-box">
-          <img
-            src={theme === "dark" ? zdaDarkLogo : zdaLightLogo}
-            alt="ZDA Z Logo"
-            title="Click to see ZDA logo page"
-            className="zda-header-logo cursor-pointer pointer-events-auto select-none transition ease-out duration-300 3xl:w-[28px] 3xl:h-[28px] 4xl:w-[36px] 4xl:h-[36px] 4k:w-[52px] 4k:h-[52px]"
-            width={28}
-            height={28}
-            onClick={() => window.location.replace("/logo")}
-            onTouchEnd={() => window.location.replace("/logo")}
-          />
-          <img
-            src={theme === "dark" ? zdaDarkHoverLogo : zdaLightHoverLogo}
-            alt="ZDA Z Logo Hover Version"
-            title="Click to see ZDA logo page"
-            className="zda-header-logo-hover cursor-pointer pointer-events-auto select-none transition ease-out duration-300 3xl:w-[28px] 3xl:h-[28px] 4xl:w-[36px] 4xl:h-[36px] 4k:w-[52px] 4k:h-[52px]"
-            width={28}
-            height={28}
-            onClick={() => window.location.replace("/logo")}
-            onTouchEnd={() => window.location.replace("/logo")}
-          />
-        </div>
-        <div className="hidden xxs:block 3xl:text-xl 4xl:text-2xl 4k:text-3xl">
-          ZeroDayAnubis
-        </div>
-        <div className="cursor-pointer focus-visible:outline-none mr-2 lg:flex lg:static lg:w-auto">
-          <ThemeMenu />
-        </div>
-      </span> */}
+      </header>
     </>
   );
 };
