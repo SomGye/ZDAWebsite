3f86e40e65c36c0eeff0f09c9fd195e4e5ed3abe
SomGye <crawfordmaxwell24@gmail.com>
2024-03-13 21:13:35 -0400

    chore: enable pjs 600 italic

--- src/main.tsx
+++ src/main.tsx
@@ -25,7 +25,7 @@ import "@fontsource/plus-jakarta-sans/400-italic.css";
 import "@fontsource/plus-jakarta-sans/400.css";
 // import "@fontsource/plus-jakarta-sans/500-italic.css";
 // import "@fontsource/plus-jakarta-sans/500.css";
-// import "@fontsource/plus-jakarta-sans/600-italic.css";
+import "@fontsource/plus-jakarta-sans/600-italic.css";
 import "@fontsource/plus-jakarta-sans/600.css";
 // import "@fontsource/plus-jakarta-sans/700-italic.css";
 // import "@fontsource/plus-jakarta-sans/700.css";
