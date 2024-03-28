2f53336d510f7cc3d915fe92bc64d845413f23bb
SomGye <crawfordmaxwell24@gmail.com>
2024-03-27 22:56:41 -0400

    feat: showcase pt 2 in Home and text tweak, pageless PageIndicator prop

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -8,6 +8,7 @@ import {
   altTheHarbingerWakes,
   altWispsOfAfterlife,
 } from "../AltText";
+import PageIndicator from "../components/PageIndicator";
 
 const HomePage = () => {
   const [, setPage] = useRecoilState(pageAtom);
@@ -33,8 +34,8 @@ const HomePage = () => {
         >
           Portfolio
         </p>
-        &nbsp; to <br className="block sm:hidden" /> see my favorite abstract
-        works ...
+        &nbsp; to <br className="block sm:hidden" /> see highlights of my
+        abstract works ...
         <br />
         <br />
         ... or you can view my current <br className="block sm:hidden" /> &nbsp;
@@ -59,7 +60,7 @@ const HomePage = () => {
         </p>
         &nbsp; too!
       </div>
-      <div className="home-page-showcase1 w-full sm:w-11/12 flex flex-col md:flex-row justify-center items-center mx-4 my-6 border border-solid md:border-none rounded-xl border-slate-300/50 dark:border-slate-700/20 shadow-[8px_4px_12px_4px_rgba(51,65,85,0.10)] dark:shadow-[0_0_12px_4px_rgba(203,213,225,0.025)] md:shadow-none dark:md:shadow-none">
+      <div className="home-page-showcase1 w-full sm:w-11/12 flex flex-col md:flex-row justify-center items-center mx-4 my-6 border border-solid md:border-none rounded-md border-slate-300/50 dark:border-slate-700/20 shadow-[8px_4px_12px_4px_rgba(51,65,85,0.10)] dark:shadow-[0_0_12px_4px_rgba(203,213,225,0.025)] md:shadow-none dark:md:shadow-none">
         <div className="home-page-showcase-img-container flex flex-col md:flex-row flex-wrap md:grid md:grid-cols-2 2xl:flex justify-center items-center gap-0 md:gap-2 px-0 xs:px-8 py-4 md:py-8 2xl:py-4 my-1 sm:my-0 border-none md:border-solid border rounded-md border-slate-300/50 dark:border-slate-700/20 drop-shadow-md shadow-none md:shadow-[8px_4px_12px_4px_rgba(51,65,85,0.10)] dark:md:shadow-[0_0_12px_4px_rgba(203,213,225,0.025)]">
           <img
             src="/assets/2023-09-17-Anubis_-_The_Deathless-nozda-600px-c1-blur3x.jpg"
@@ -119,7 +120,44 @@ const HomePage = () => {
           />
         </div>
       </div>
-      {/* TODO: showcase part 2? */}
+      <PageIndicator pageless />
+      <div className="home-page-showcase2 w-full sm:w-11/12 flex flex-col md:flex-row justify-center items-center mx-4 my-6 border border-solid md:border-none rounded-md border-slate-300/50 dark:border-slate-700/20 shadow-[8px_4px_12px_4px_rgba(51,65,85,0.10)] dark:shadow-[0_0_12px_4px_rgba(203,213,225,0.025)] md:shadow-none dark:md:shadow-none">
+        <div className="home-page-showcase2-img-container-sm flex 2xl:hidden flex-col justify-center items-center gap-0 md:gap-2 px-0 xs:px-8 py-4 md:py-8 2xl:py-4 my-1 sm:my-0 border-none md:border-solid border rounded-md border-slate-300/50 dark:border-slate-700/20 drop-shadow-md shadow-none md:shadow-[8px_4px_12px_4px_rgba(51,65,85,0.10)] dark:md:shadow-[0_0_12px_4px_rgba(203,213,225,0.025)]">
+          <img
+            src="/assets/out_of_shape-20240218_1209-document_blue-1040px-c1-blur3x.jpg"
+            alt={""}
+            title="Out of Shape (Document Version)"
+            className="home-img05b z-20 h-full max-w-64 sm:max-w-[320px] md:max-w-[460px] lg:max-w-[520px] my-4 object-cover object-center rounded-md select-none"
+            loading="lazy"
+          />
+          <img
+            onLoad={() => loadImgHandler("home-img05")}
+            src="/assets/out_of_shape-20240218_1209-document_blue-1040px-c1.jpg"
+            alt={""}
+            title="Out of Shape (Document Version)"
+            className="hidden home-img05 z-10 h-full max-w-64 sm:max-w-[320px] md:max-w-[460px] lg:max-w-[520px] my-4 object-cover object-center rounded-md brightness-[.96] border-solid border dark:border-2 border-transparent transition-all duration-200 ease-out hover:brightness-[1.01] hover:border-neutral-400/50 dark:hover:border-neutral-500/60 hover:transition-all hover:duration-200 hover:ease-out select-none"
+          />
+        </div>
+        <div className="home-page-showcase2-img-container hidden 2xl:flex flex-row justify-center items-center gap-0 md:gap-2 px-0 xs:px-8 py-4 md:py-8 2xl:py-4 my-1 sm:my-0 border-none md:border-solid border rounded-md border-slate-300/50 dark:border-slate-700/20 drop-shadow-md shadow-none md:shadow-[8px_4px_12px_4px_rgba(51,65,85,0.10)] dark:md:shadow-[0_0_12px_4px_rgba(203,213,225,0.025)]">
+          <img
+            src="/assets/2024-02-07-Nonconformant_Spectrum-1000px-c1-blur3x.jpg"
+            alt={""}
+            title="Non-conformant Spectrum"
+            className="home-img06b z-20 h-full max-w-[1000px] my-4 object-cover object-center rounded-md select-none"
+            loading="lazy"
+          />
+          <img
+            onLoad={() => loadImgHandler("home-img06")}
+            src="/assets/2024-02-07-Nonconformant_Spectrum-1000px-c1.jpg"
+            alt={""}
+            title="Non-conformant Spectrum"
+            className="hidden home-img06 z-10 h-full max-w-[1000px] my-4 object-cover object-center rounded-md brightness-[.96] border-solid border dark:border-2 border-transparent transition-all duration-200 ease-out hover:brightness-[1.01] hover:border-neutral-400/50 dark:hover:border-neutral-500/60 hover:transition-all hover:duration-200 hover:ease-out select-none"
+          />
+        </div>
+      </div>
+      <div className="home-page-outro max-w-[90%] italic text-base mt-10 sm:mt-14">
+        Thank you for visiting!
+      </div>
     </div>
   );
 };
