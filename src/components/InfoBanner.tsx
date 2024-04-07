1ba6acc6fb68bca7dc12959f08fc42e95399c791
SomGye <crawfordmaxwell24@gmail.com>
2024-04-07 14:14:13 -0400

    feat: Vercel KV API for comm slots, todo spinners

--- src/components/InfoBanner.tsx
+++ src/components/InfoBanner.tsx
@@ -1,7 +1,131 @@
 import * as React from "react";
 import PageLinks from "./PageLinks";
+import { useRecoilState, useRecoilValue } from "recoil";
+import {
+  activeSlotsAtom,
+  commsOpenAtom,
+  commStatusAtom,
+  maxSlotsAtom,
+  slotsReadyAtom,
+  waitlistSlotsAtom,
+} from "../states/commsAtom";
 
 const InfoBanner = () => {
+  const [activeSlots, setActiveSlots] = useRecoilState(activeSlotsAtom);
+  const [waitSlots, setWaitSlots] = useRecoilState(waitlistSlotsAtom);
+  const maxSlots = useRecoilValue(maxSlotsAtom);
+  const [commStatus, setCommStatus] = useRecoilState(commStatusAtom);
+  const [, setCommsOpen] = useRecoilState(commsOpenAtom);
+  const [, setSlotsReady] = useRecoilState(slotsReadyAtom);
+  const [activeSlotsReady, setActiveSlotsReady] = React.useState(false);
+  const [waitSlotsReady, setWaitSlotsReady] = React.useState(false);
+  const [statusReady, setStatusReady] = React.useState(false);
+  const slotsFallback = 0;
+  const slotsInit = -1;
+  const statusInit = "";
+  const statusOpen = "OPEN";
+  const statusClosed = "CLOSED";
+
+  React.useEffect(() => {
+    // Query Vercel KV Store to get Commissions Slots Info
+    const fetchData = async () => {
+      const result = await fetch(
+        `${
+          import.meta.env.VITE_KV_REST_API_URL
+        }/mget/slots:active/slots:waitlist`,
+        {
+          headers: {
+            Authorization: `Bearer ${import.meta.env.VITE_KV_REST_API_TOKEN}`,
+          },
+        }
+      )
+        .then((response) => response.json())
+        .catch((err) => console.log(err));
+
+      // Check for Result/Error
+      if (result && !result.error && result.result) {
+        // Check for empty results
+        if (result.result.length === 0) {
+          console.log("Warning while fetching KV: No results");
+        } else {
+          // Parse Current Active and Waitlist Slots
+          let commSlotsResult = activeSlots;
+          let waitSlotsResult = waitSlots;
+          if (result.result[0] && result.result[0].length > 0) {
+            commSlotsResult = parseInt(result.result[0]);
+          }
+          if (result.result[1] && result.result[1].length > 0) {
+            waitSlotsResult = parseInt(result.result[1]);
+          }
+
+          // Update Commission Slots
+          if (!isNaN(commSlotsResult)) {
+            setActiveSlots(commSlotsResult);
+          }
+          if (!isNaN(waitSlotsResult)) {
+            setWaitSlots(waitSlotsResult);
+          }
+
+          // Determine Commission Status
+          if (commSlotsResult < 1) {
+            setCommStatus(statusClosed);
+          } else {
+            setCommStatus(statusOpen);
+          }
+
+          // Log result
+          console.log("Commission Slots updated from KV result:");
+          console.log(result.result);
+        }
+      } else if (result && result.error) {
+        // ERROR
+        console.log("Error while fetching KV: " + result.error);
+        setSlotsReady(true);
+      } else if (!result) {
+        // FETCH ERROR
+        console.log(
+          "Unexpected Error while fetching KV: Failed to Fetch Due to Incorrect URL"
+        );
+        setSlotsReady(true);
+      } else {
+        // UNEXPECTED ERROR
+        console.log("Unexpected Error while fetching KV: " + result);
+        setSlotsReady(true);
+      }
+    };
+
+    // Call Vercel KV API within hook
+    // TODO: comment out to stop API hits temporarily until ready
+    fetchData();
+  }, []);
+
+  React.useEffect(() => {
+    // Set ready only when not the init value
+    if (activeSlots > slotsInit) {
+      setActiveSlotsReady(true);
+    }
+  }, [activeSlots]);
+
+  React.useEffect(() => {
+    // Set ready only when not the init value
+    if (waitSlots > slotsInit) {
+      setWaitSlotsReady(true);
+    }
+  }, [waitSlots]);
+
+  React.useEffect(() => {
+    // Set ready only when not the init value
+    if (commStatus !== statusInit) {
+      setStatusReady(true);
+      // Update boolean state
+      if (commStatus === statusClosed) {
+        setCommsOpen(false);
+      } else {
+        setCommsOpen(true);
+      }
+    }
+  }, [commStatus]);
+
   return (
     <div className="info-banner-container w-full py-2 lg:py-0 flex flex-col lg:flex-row justify-center items-center bg-gradient-to-b from-[rgb(255_0_72_/_0.045)] dark:from-[rgb(255_0_72_/_0.035)] to-transparent">
       <div className="info-zda-container">
@@ -10,19 +134,26 @@ const InfoBanner = () => {
         </span>
       </div>
       <div className="comm-info-container w-11/12 lg:mt-4">
+        {/* COMM STATUS - DEFAULT CLOSED */}
         <div className="comm-status-container m-2">
           <span className="font-medium tracking-wide text-sm text-slate-900 dark:text-slate-200 pointer-events-none select-none">
-            COMMISSIONS CLOSED
+            COMMISSIONS {statusReady ? commStatus : statusClosed}
           </span>
         </div>
+        {/* ACTIVE COMM SLOTS */}
         <div className="comm-slots-container m-2">
           <span className="font-light italic tracking-wide text-sm text-slate-900 dark:text-slate-200 pointer-events-none select-none">
-            {"(0/5 SLOTS)"}
+            {`(${
+              activeSlotsReady ? activeSlots : slotsFallback
+            }/${maxSlots} SLOTS)`}
           </span>
         </div>
+        {/* WAITLIST COMM SLOTS */}
         <div className="comm-waitlist-container m-2">
           <span className="font-light italic tracking-wide text-sm text-slate-900 dark:text-slate-200 pointer-events-none select-none">
-            {"(0/5 WAITLIST)"}
+            {`(${
+              waitSlotsReady ? waitSlots : slotsFallback
+            }/${maxSlots} WAITLIST)`}
           </span>
         </div>
       </div>
