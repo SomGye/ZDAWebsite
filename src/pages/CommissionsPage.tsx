6779b9b86d3d62e4569c488e5af7f4367cc7704a
SomGye <crawfordmaxwell24@gmail.com>
2024-06-04 20:01:11 -0400

    fix: comm form btn add dark to mobile btn too and tweak desktop btn dims

--- src/pages/CommissionsPage.tsx
+++ src/pages/CommissionsPage.tsx
@@ -25,14 +25,14 @@ const CommissionsPage = () => {
       <span className="text-lg font-medium">TODO: Commissions Page</span>
       {/* Desktop/Tablet Only - Show/Hide Form directly in page */}
       <button
-        className="hidden md:block w-56 h-8 m-4 rounded-2xl bg-zdaRedpink-600 hover:bg-zdaRedpink-700/90 active:bg-zdaRedpink-800 dark:bg-white/90 dark:hover:bg-neutral-300/80 dark:active:bg-neutral-500 text-white/95 hover:text-white dark:text-gray-950 dark:hover:text-black font-outfit drop-shadow-btn-light dark:drop-shadow-btn-dark hover:drop-shadow-sm dark:hover:drop-shadow-btn-dark-sm motion-safe:transition-all motion-safe:duration-300 ease-out"
+        className="hidden md:block w-[14.5rem] h-[2.25rem] m-4 p-1 rounded-2xl bg-zdaRedpink-600 hover:bg-zdaRedpink-700/90 active:bg-zdaRedpink-800 dark:bg-white/90 dark:hover:bg-neutral-300/80 dark:active:bg-neutral-500 text-white/95 hover:text-white dark:text-gray-950 dark:hover:text-black font-outfit drop-shadow-btn-light dark:drop-shadow-btn-dark hover:drop-shadow-sm dark:hover:drop-shadow-btn-dark-sm motion-safe:transition-all motion-safe:duration-200 ease-out"
         onClick={() => setFormVisible(!formVisible)}
       >
         {formVisible ? "Hide Commissions Form" : "Show Commissions Form"}
       </button>
       {/* Mobile Only - Open form in new tab */}
       <button
-        className="block md:hidden w-64 h-8 m-4 rounded-2xl bg-zdaRedpink-650 hover:bg-zdaRedpink-500 active:bg-zdaRedpink-700 text-zdaRedpink-50 font-outfit motion-safe:transition-all motion-safe:duration-200 ease-out"
+        className="block md:hidden w-64 h-10 m-4 rounded-2xl bg-zdaRedpink-600 hover:bg-zdaRedpink-700/90 active:bg-zdaRedpink-800 dark:bg-white/90 dark:hover:bg-neutral-300/80 dark:active:bg-neutral-500 text-white/95 hover:text-white dark:text-gray-950 dark:hover:text-black font-outfit text-lg drop-shadow-btn-light dark:drop-shadow-btn-dark hover:drop-shadow-sm dark:hover:drop-shadow-btn-dark-sm motion-safe:transition-all motion-safe:duration-200 ease-out"
         onClick={() => clickLink(formLink)}
       >
         Commissions Form
