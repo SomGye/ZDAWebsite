aa3aea4e83ac16f80b8fffa94e2f7c397397f840
SomGye <crawfordmaxwell24@gmail.com>
2024-06-08 10:45:03 -0400

    tweak: refined CardBreak colors

--- src/components/CardBreak.tsx
+++ src/components/CardBreak.tsx
@@ -2,11 +2,10 @@ import * as React from "react";
 
 const CardBreak = () => {
   return (
-    // TODO: refine colors, especially for light mode
     <>
       <div className="long-fading-line-container flex flex-row justify-center items-center w-full">
-        <div className="left-fading-line w-6/12 h-px my-0 rounded-2xl bg-gradient-to-r from-slate-300 to-slate-500 dark:from-neutral-900 dark:to-neutral-700" />
-        <div className="right-fading-line w-6/12 h-px my-0 rounded-2xl bg-gradient-to-l from-slate-300 to-slate-500 dark:from-neutral-900 dark:to-neutral-700" />
+        <div className="left-fading-line w-6/12 h-px my-0 rounded-2xl bg-gradient-to-r from-transparent to-slate-500/30 dark:from-transparent dark:to-neutral-900/30" />
+        <div className="right-fading-line w-6/12 h-px my-0 rounded-2xl bg-gradient-to-l from-transparent to-slate-500/30 dark:from-transparent dark:to-neutral-900/30" />
       </div>
     </>
   );
