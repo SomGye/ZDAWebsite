71b76bb7b1d8ecbb2b7d412c0f00a5b8a6af316b
SomGye <crawfordmaxwell24@gmail.com>
2024-06-18 15:09:06 -0400

    fix: hacky workaround for CardBreak on 5k scaling

--- src/components/CardBreak.tsx
+++ src/components/CardBreak.tsx
@@ -13,7 +13,7 @@ const CardBreak = ({ isVertical, isColorful }: props) => {
           {/* Default */}
           <div
             className={
-              "long-fading-line-container z-10 absolute flex flex-row justify-center items-center w-full motion-safe:transition-all motion-safe:duration-500 ease-out" +
+              "long-fading-line-container z-10 absolute flex flex-row justify-center items-center w-full h-auto 5k:h-[3px] motion-safe:transition-all motion-safe:duration-500 ease-out" +
               (isColorful ? " opacity-0" : " opacity-100")
             }
           >
@@ -23,7 +23,7 @@ const CardBreak = ({ isVertical, isColorful }: props) => {
           {/* Hover Overlay (Colorful) */}
           <div
             className={
-              "long-fading-line-container-colorful z-20 absolute flex flex-row justify-center items-center w-full motion-safe:transition-all motion-safe:duration-500 ease-out" +
+              "long-fading-line-container-colorful z-20 absolute flex flex-row justify-center items-center w-full h-auto 5k:h-[3px] motion-safe:transition-all motion-safe:duration-500 ease-out" +
               (isColorful ? " opacity-100" : " opacity-0")
             }
           >
@@ -33,7 +33,7 @@ const CardBreak = ({ isVertical, isColorful }: props) => {
         </div>
       )}
       {isVertical && (
-        <div className="long-fading-line-container flex flex-col justify-center items-center w-full h-8 mx-2">
+        <div className="long-fading-line-container flex flex-col justify-center items-center w-full 5k:w-[2px] h-8 mx-2">
           <div className="top-fading-line w-px h-4 mx-1 rounded-2xl bg-gradient-to-b from-gray-400/40 to-gray-400/80 dark:from-neutral-900/25 dark:to-neutral-900" />
           <div className="bottom-fading-line w-px h-4 mx-1 rounded-2xl bg-gradient-to-t from-gray-400/40 to-gray-400/80 dark:from-neutral-900/25 dark:to-neutral-900" />
         </div>
