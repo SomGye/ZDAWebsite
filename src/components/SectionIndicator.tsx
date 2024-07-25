d1c234bf23b830628bb4a7b2483a30960acc9cf1
SomGye <crawfordmaxwell24@gmail.com>
2024-07-25 12:57:00 -0400

    fix: add whitespace nowrap to SectionIndicator for FF

--- src/components/SectionIndicator.tsx
+++ src/components/SectionIndicator.tsx
@@ -12,7 +12,7 @@ const SectionIndicator = ({ sectionName, id }: props) => {
       id={id ? id : sectionName}
     >
       <div className="left-fading-line w-1/2 lg:w-16 2xl:w-12 h-px my-1 rounded-2xl bg-gradient-to-r from-slate-100 to-slate-500 dark:from-gray-800 dark:to-gray-400" />
-      <div className="mid-line w-auto mx-3 2xl:mx-2 text-xl font-extrabold text-nowrap text-slate-600 dark:text-slate-300 select-none">
+      <div className="mid-line w-auto mx-3 2xl:mx-2 text-xl font-extrabold text-nowrap whitespace-nowrap text-slate-600 dark:text-slate-300 select-none">
         {sectionName}
       </div>
       <div className="right-fading-line w-1/2 lg:w-full h-px my-1 rounded-2xl bg-gradient-to-l from-slate-200 to-slate-500 dark:from-gray-900 dark:to-gray-400" />
