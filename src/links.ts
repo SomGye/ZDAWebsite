4a08b76e4093a05048d5223ac4504876181d4d2e
Anubis Zero <crawfordmaxwell24@gmail.com>
2024-09-28 19:10:22 -0400

    Tweak: Use Dynamic Short IO Links (#67)
    tweak: use the short io links instead

--- src/links.ts
+++ src/links.ts
@@ -1,8 +1,8 @@
 /* GLOBALLY USED LINKS */
 // Redirects and ZDA-related links
-export const commFormLink = "https://tally.so/r/w52VP6";
-export const commInfoLink = "https://drive.google.com/file/d/1pzzikwvD9myZus1P3TX0CLwe7Vp6njwa/view?usp=sharing";
-export const privacyLink = "https://drive.google.com/file/d/13cQYluebHO55mcJ057e2Z2IMw3ceW5rM/view?usp=sharing";
+export const commFormLink = "https://go.zda.works/commform";
+export const commInfoLink = "https://go.zda.works/comminfo";
+export const privacyLink = "https://go.zda.works/privacyzda";
 export const zdaWorksLink = "https://www.zda.works/";
 // Social Media
 export const bskyLink = "https://bsky.app/profile/zerodayanubis.com";
@@ -10,7 +10,7 @@ export const igLink = "https://instagram.com/zerodayanubis?igshid=OGQ5ZDc2ODk2ZA
 export const threadsLink = "https://www.threads.net/@zerodayanubis";
 export const caraLink = "https://cara.app/zerodayanubis/";
 // Prints/Support
-export const printShopLink = "https://www.inprnt.com/gallery/zerodayanubis/";
+export const printShopLink = "https://go.zda.works/prints";
 export const kofiLink = "https://ko-fi.com/zerodayanubis";
 export const payPalLink = "https://www.paypal.com/ncp/payment/AGHU59JTKAC66";
 export const venmoLink = "https://account.venmo.com/u/somgye";
