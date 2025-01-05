948e2a75b01d86f9e3126c3fc08a3a1d8a83e2f2
SomGye <crawfordmaxwell24@gmail.com>
2025-01-04 21:36:59 -0500

    feat: add theme atom and options, new blue logo

--- src/components/ThemeMenu.tsx
+++ src/components/ThemeMenu.tsx
@@ -1,5 +1,5 @@
 import * as React from "react";
-import { themeAtom } from "../states/themeAtom";
+import { colorSchemeAtom, themeAtom } from "../states/themeAtom";
 import { useRecoilState } from "recoil";
 import * as DropdownMenu from "@radix-ui/react-dropdown-menu";
 import {
@@ -8,13 +8,55 @@ import {
   lightIcon,
   lightMenuIcon,
   sysMenuIcon,
+  colorSchemeMenuIcon,
 } from "../icons";
+import { capitalizeString } from "../helpers";
+
+const defaultTheme = "system";
+const defaultColorScheme = "blue";
+const colorSchemes = ["blue", "red"];
+const triggerClasses = [
+  {
+    className:
+      "rounded-full w-7 h-7 inline-flex items-center justify-center text-zdaBlue-600 dark:text-zdaBlue-50/60 bg-zdaBG-lightCard dark:bg-zdaBG-darkCard shadow-[0_1px_8px] shadow-stone-400 hover:shadow-stone-400/25 dark:shadow-zdaBlue-50/15 dark:hover:shadow-zdaBlue-50/5 outline outline-1 outline-zdaBlue-500/25 dark:outline-zdaBlue-50/10 hover:bg-zdaBlue-500/25 dark:hover:bg-zdaBlue-100/20 active:bg-zdaBlue-400/50 dark:active:bg-zdaBlue-50/25 motion-safe:transition ease-out motion-safe:duration-300 webkit-appearance-none appearance-none",
+    colorScheme: colorSchemes[0],
+  },
+  {
+    className:
+      "rounded-full w-7 h-7 inline-flex items-center justify-center text-zdaRedpink-600 dark:text-zdaRedpink-50/60 bg-zdaBG-lightCard dark:bg-zdaBG-darkCard shadow-[0_1px_8px] shadow-stone-400 hover:shadow-stone-400/25 dark:shadow-zdaRedpink-50/15 dark:hover:shadow-zdaRedpink-50/5 outline outline-1 outline-zdaRedpink-500/25 dark:outline-zdaRedpink-50/10 hover:bg-zdaRedpink-500/25 dark:hover:bg-zdaRedpink-100/20 active:bg-zdaRedpink-400/50 dark:active:bg-zdaRedpink-50/25 motion-safe:transition ease-out motion-safe:duration-300 webkit-appearance-none appearance-none",
+    colorScheme: colorSchemes[1],
+  },
+];
+const itemClasses = [
+  {
+    className:
+      "group flex items-center relative h-6 my-2 md:my-0 px-6 py-4 4xl:text-lg text-zdaBlue-600/90 dark:text-zdaBlue-50/75 rounded-md 3xl:rounded-lg select-none outline-none data-[highlighted]:bg-zdaBlue-600/75 data-[highlighted]:text-neutral-50 dark:data-[highlighted]:bg-zdaBlue-50/75 dark:data-[highlighted]:text-zinc-950 motion-safe:transition-colors ease-out motion-safe:duration-200",
+    colorScheme: colorSchemes[0],
+  },
+  {
+    className:
+      "group flex items-center relative h-6 my-2 md:my-0 px-6 py-4 4xl:text-lg text-zdaRedpink-600/90 dark:text-zdaRedpink-50/75 rounded-md 3xl:rounded-lg select-none outline-none data-[highlighted]:bg-zdaRedpink-600/75 data-[highlighted]:text-neutral-50 dark:data-[highlighted]:bg-zdaRedpink-50/75 dark:data-[highlighted]:text-zinc-950 motion-safe:transition-colors ease-out motion-safe:duration-200",
+    colorScheme: colorSchemes[1],
+  },
+];
+
+const separatorClasses = [
+  {
+    className: "h-[1px] m-1 bg-zdaBlue-600/30 dark:bg-zdaBlue-100/20",
+    colorScheme: colorSchemes[0],
+  },
+  {
+    className: "h-[1px] m-1 bg-zdaRedpink-600/30 dark:bg-zdaRedpink-100/20",
+    colorScheme: colorSchemes[1],
+  },
+];
 
 const ThemeMenu = () => {
   const [theme, setTheme] = useRecoilState(themeAtom);
+  const [colorScheme, setColorScheme] = useRecoilState(colorSchemeAtom);
   const [isAutoMode, setAutoMode] = React.useState(true);
   const [open, setOpen] = React.useState(false);
-  const [option, setOption] = React.useState("system"); // dark/light/system
+  const [sysOption, setOption] = React.useState("system"); // dark/light/system
   const systemMatch = window.matchMedia("(prefers-color-scheme: dark)");
 
   const changeTheme = (newTheme: string) => {
@@ -38,7 +80,7 @@ const ThemeMenu = () => {
       setOption("light");
     } else {
       // Set prefs
-      localStorage.setItem("theme", "system");
+      localStorage.setItem("theme", defaultTheme);
       // Set body style and vars
       if (systemMatch.matches) {
         document.documentElement.classList.add("dark");
@@ -48,15 +90,33 @@ const ThemeMenu = () => {
         setTheme("light");
       }
       console.log("Theme set to system from menu");
-      setOption("system");
+      setOption(defaultTheme);
     }
     setOpen(false);
   };
 
-  const detectColorScheme = () => {
-    // Get theme preference from user (default: system)
-    const storedTheme = localStorage.getItem("theme") || "system";
-    // Set theme and option from detected Theme
+  const changeColorScheme = (newColorScheme: string) => {
+    if (colorSchemes.includes(newColorScheme)) {
+      // Set prefs
+      localStorage.setItem("colorScheme", newColorScheme);
+
+      // Set vars
+      setColorScheme(newColorScheme);
+      console.log("Color Scheme set to " + newColorScheme + " from menu");
+    } else {
+      // Set prefs
+      localStorage.setItem("colorScheme", defaultColorScheme);
+
+      // Set body style and vars
+      setColorScheme(newColorScheme);
+    }
+    setOpen(false);
+  };
+
+  const detectTheme = () => {
+    // Get theme preference from user
+    const storedTheme = localStorage.getItem("theme") || defaultTheme;
+    // Set theme and sysOption from detected Theme
     if (storedTheme === "dark") {
       document.documentElement.classList.add("dark");
       setOption(storedTheme);
@@ -67,7 +127,7 @@ const ThemeMenu = () => {
       setTheme(storedTheme);
     } else {
       // Set system theme for next time
-      localStorage.setItem("theme", "system");
+      localStorage.setItem("theme", defaultTheme);
       // Use matchMedia to get system theme
       if (systemMatch.matches) {
         document.documentElement.classList.add("dark");
@@ -76,7 +136,43 @@ const ThemeMenu = () => {
         document.documentElement.classList.remove("dark");
         setTheme("light");
       }
-      setOption("system");
+      setOption(defaultTheme);
+    }
+  };
+
+  const detectColorScheme = () => {
+    // Get color scheme preference from user
+    const storedScheme =
+      localStorage.getItem("colorScheme") || defaultColorScheme;
+    // Set color scheme from detected storage
+    setColorScheme(storedScheme);
+
+    // Set default color scheme for next time
+    if (!colorSchemes.includes(storedScheme)) {
+      // Set default color scheme for next time
+      localStorage.setItem("colorScheme", defaultColorScheme);
+    }
+  };
+
+  const getColorSchemeClassName = (element: string) => {
+    let targetClasses;
+    // Determine class list from element type
+    if (element === "trigger") {
+      targetClasses = triggerClasses;
+    } else if (element === "separator") {
+      targetClasses = separatorClasses;
+    } else {
+      targetClasses = itemClasses;
+    }
+
+    // Filter on classes for className matching colorScheme
+    const resultObj = targetClasses.filter(
+      (classObj) => classObj.colorScheme === colorScheme
+    );
+    if (resultObj) {
+      return resultObj[0].className;
+    } else {
+      return targetClasses[0].className;
     }
   };
 
@@ -97,20 +193,23 @@ const ThemeMenu = () => {
   };
 
   React.useEffect(() => {
-    // Detect preferred Color Scheme from user or system
+    // Detect preferred Theme from user or system
+    detectTheme();
+
+    // Detect preferred Color Scheme from user
     detectColorScheme();
   }, []);
 
   React.useEffect(() => {
     // Check for system option
-    if (option === "system") {
+    if (sysOption === "system") {
       // Turn on Auto Mode
       setAutoMode(true);
     } else {
       // Turn off Auto Mode
       setAutoMode(false);
     }
-  }, [option]);
+  }, [sysOption]);
 
   React.useEffect(() => {
     if (isAutoMode) {
@@ -138,7 +237,7 @@ const ThemeMenu = () => {
           {/* Button Section */}
           <DropdownMenu.Trigger asChild>
             <button
-              className="rounded-full w-7 h-7 inline-flex items-center justify-center text-zdaRedpink-600 dark:text-zdaRedpink-50/60 bg-zdaBG-lightCard dark:bg-zdaBG-darkCard shadow-[0_1px_8px] shadow-stone-400 hover:shadow-stone-400/25 dark:shadow-zdaRedpink-50/15 dark:hover:shadow-zdaRedpink-50/5 outline outline-1 outline-zdaRedpink-500/25 dark:outline-zdaRedpink-50/10 hover:bg-zdaRedpink-500/25 dark:hover:bg-zdaRedpink-100/20 active:bg-zdaRedpink-400/50 dark:active:bg-zdaRedpink-50/25 motion-safe:transition ease-out motion-safe:duration-300 webkit-appearance-none appearance-none"
+              className={getColorSchemeClassName("trigger")}
               aria-label="Theme Menu"
               title="Theme Menu"
               onClick={() => setOpen((prev) => !prev)}
@@ -149,32 +248,56 @@ const ThemeMenu = () => {
           {/* Menu Section */}
           <DropdownMenu.Portal>
             <DropdownMenu.Content
+              // TODO: revisit shadow-theme-menu-dropdown light and dark
               className="z-50 min-w-40 bg-zdaBG-lighterCard dark:bg-zdaBG-darkerCard rounded-lg p-2 shadow-theme-menu-dropdown-light dark:shadow-theme-menu-dropdown-dark will-change-[opacity,transform] data-[side=top]:animate-slideDownAndFade data-[side=right]:animate-slideLeftAndFade data-[side=bottom]:animate-slideUpAndFade data-[side=left]:animate-slideRightAndFade"
               sideOffset={5}
               align="start"
               onInteractOutside={() => setOpen(false)}
             >
+              {/* TODO: use mapping instead! */}
+              {/* Theme Items */}
               <DropdownMenu.Item
-                className="group flex items-center relative h-6 my-2 md:my-0 px-6 py-4 4xl:text-lg text-zdaRedpink-600/90 dark:text-zdaRedpink-50/75 rounded-md 3xl:rounded-lg select-none outline-none data-[highlighted]:bg-zdaRedpink-600/75 data-[highlighted]:text-neutral-50 dark:data-[highlighted]:bg-zdaRedpink-50/75 dark:data-[highlighted]:text-zinc-950 motion-safe:transition-colors ease-out motion-safe:duration-200"
+                className={getColorSchemeClassName("item")}
                 onClick={() => changeTheme("dark")}
               >
                 <span className="mr-2 4xl:mr-3 4k:mr-4">{darkMenuIcon}</span>
                 Dark
               </DropdownMenu.Item>
               <DropdownMenu.Item
-                className="group flex items-center relative h-6 my-2 md:my-0 px-6 py-4 4xl:text-lg text-zdaRedpink-600/90 dark:text-zdaRedpink-50/75 rounded-md 3xl:rounded-lg select-none outline-none data-[highlighted]:bg-zdaRedpink-600/75 data-[highlighted]:text-neutral-50 dark:data-[highlighted]:bg-zdaRedpink-50/75 dark:data-[highlighted]:text-zinc-950 motion-safe:transition-colors ease-out motion-safe:duration-200"
+                className={getColorSchemeClassName("item")}
                 onClick={() => changeTheme("light")}
               >
                 <span className="mr-2 4xl:mr-3 4k:mr-4">{lightMenuIcon}</span>
                 Light
               </DropdownMenu.Item>
-              <DropdownMenu.Separator className="h-[1px] m-1 bg-zdaRedpink-600/20 dark:bg-gray-600/5" />
               <DropdownMenu.Item
-                className="group flex items-center relative h-6 my-2 md:my-0 px-6 py-4 4xl:text-lg text-zdaRedpink-600/90 dark:text-zdaRedpink-50/75 rounded-md 3xl:rounded-lg select-none outline-none data-[highlighted]:bg-zdaRedpink-600/75 data-[highlighted]:text-neutral-50 dark:data-[highlighted]:bg-zdaRedpink-50/75 dark:data-[highlighted]:text-zinc-950 motion-safe:transition-colors ease-out motion-safe:duration-200"
+                className={getColorSchemeClassName("item")}
                 onClick={() => changeTheme("system")}
               >
                 <span className="mr-2 4xl:mr-3 4k:mr-4">{sysMenuIcon}</span>
                 System
+              </DropdownMenu.Item>
+              <DropdownMenu.Separator
+                className={getColorSchemeClassName("separator")}
+              />
+              {/* Color Scheme Items */}
+              <DropdownMenu.Item
+                className={getColorSchemeClassName("item")}
+                onClick={() => changeColorScheme(colorSchemes[0])}
+              >
+                <span className="mr-2 4xl:mr-3 4k:mr-4">
+                  {colorSchemeMenuIcon}
+                </span>
+                {capitalizeString(colorSchemes[0])}
+              </DropdownMenu.Item>
+              <DropdownMenu.Item
+                className={getColorSchemeClassName("item")}
+                onClick={() => changeColorScheme(colorSchemes[1])}
+              >
+                <span className="mr-2 4xl:mr-3 4k:mr-4">
+                  {colorSchemeMenuIcon}
+                </span>
+                {capitalizeString(colorSchemes[1])}
               </DropdownMenu.Item>
               <DropdownMenu.Arrow className="fill-zdaBG-lighterCard dark:fill-zdaRedpink-1000" />
             </DropdownMenu.Content>
--- src/states/themeAtom.ts
+++ src/states/themeAtom.ts
@@ -2,5 +2,10 @@ import { atom } from "recoil";
 
 export const themeAtom = atom({
   key: "ThemeState",
-  default: "light", //"system",
+  default: "light",
+});
+
+export const colorSchemeAtom = atom({
+  key: "ColorSchemeState",
+  default: "blue", // supported: "blue", "red"
 });
